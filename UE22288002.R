15db69b2f75203718368ed8fc01a95c252be5e35
Marie Ann De Los Santos <marieanndlsj@gmail.com>
2022-12-13 14:16:39 +0100

    updated practica

--- UE22288002.R
+++ UE22288002.R
@@ -1,6 +1,6 @@
 
 # Modulo II ---------------------------------------------------------------
-
+#A
 # i -----------------------------------------------------------------------
 
 url_='https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/'
@@ -43,7 +43,7 @@ ds_lowcost %>% select(precio_gasoleo_a,precio_gasolina_95_e5, idccaa, rotulo) %>
 
 ds_lowcost %>% count(horario,sort = TRUE)
 
-no_24h <- ds_lowcost %>% filter(horario == 'L-D: 24H') %>%  select(!horario)
+
 
 ## iv --------------------------------------------------------------------
 leaflet::addTiles()
@@ -52,3 +52,7 @@ no_24h %>% leaflet() %>% addTiles() %>% addCircleMarkers(lng = ~longitud_wgs84, 
 top_ten <- no_24h %>% select(latitud, longitud_wgs84, municipio, rotulo) %>% filter(municipio == 'Alcobendas')
 no_24h %>% select(latitud, longitud_wgs84, low_cost, rotulo) %>% filter(low_cost == 'TRUE') no_lowcost[1:4, 1:10] == 'TRUE'
 
+# d
+# i ------- no 24 horas
+no_24h <- ds_lowcost %>% filter(horario == 'L-D: 24H') %>%  select(!horario) %>% view()
+write_excel_csv(no_24h)
